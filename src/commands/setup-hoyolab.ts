6ac21689ab5d6c9b33259c0bed86dfac07904d5c
Rizky Pratama <rizkypraschool@gmail.com>
2026-01-31 19:46:31 +0700

    aa

--- src/commands/setup-hoyolab.ts
+++ src/commands/setup-hoyolab.ts
@@ -20,7 +20,7 @@ export async function execute(interaction: ChatInputCommandInteraction): Promise
     const tokenInput = new TextInputBuilder()
         .setCustomId('hoyolab-token')
         .setLabel('Token (ltoken, ltuid, cookie_token...)')
-        .setPlaceholder('ltoken_v2=...; ltuid_v2=...; cookie_token_v2=...; account_id_v2=...; (Get from Application -> Cookies)')
+        .setPlaceholder('ltoken_v2=...; ltuid_v2=...; cookie_token_v2=...; account_id_v2=...;')
         .setStyle(TextInputStyle.Paragraph)
         .setRequired(true)
         .setMinLength(20);

15fe6e09d4e7e2c4c39fc45e0dd0bc6f16ed9ac2
Aruh1 <rizkypraschool@gmail.com>
2026-04-04 22:31:25 +0700

    fix: disable pino-pretty of production mode

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -3,12 +3,15 @@ import pino from "pino";
 // Configure pino logger
 export const logger = pino({
     level: process.env.NODE_ENV === "production" ? "info" : "debug",
-    transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            translateTime: "SYS:standard",
-            ignore: "pid,hostname"
-        }
-    }
+    transport:
+        process.env.NODE_ENV === "production"
+            ? undefined
+            : {
+                  target: "pino-pretty",
+                  options: {
+                      colorize: true,
+                      translateTime: "SYS:standard",
+                      ignore: "pid,hostname"
+                  }
+              }
 });
